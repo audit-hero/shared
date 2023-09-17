324842021e144ecbe786a37d2eacf40ad6eb5707
slightscan <tohohh1@gmail.com>
2023-09-17 17:12:48 +0700

    commit

--- dist/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./sentry-lambda.js";
-export * from "./utils.js";
-export * from "./types-contest.js";
-export * from "./types-finding.js";
-export * from "./types-github.js";
-export * from "./types.js";
