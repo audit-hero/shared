85d76bda7ce8a9ead9a7877767b1f235e6781a9e
totovoto <tohohh1@gmail.com>
2024-09-09 15:52:13 +0300

    update

--- src/either/transform.ts
+++ src/either/transform.ts
@@ -88,8 +88,6 @@ export let fetchTE =
 /**
  * We either stream chat response as string + return it as E.right in the end, or return the error
  * as E.left
- * 
- * 4
  *
  * @param stream - here we stream the E.right content as string before returning the E.right in the
  * end as well.
