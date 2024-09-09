dd592e2261a788d0766fc52bb461380a6ecabf69
totovoto <tohohh1@gmail.com>
2024-09-09 15:52:53 +0300

    update

--- dist/either/transform.js
+++ dist/either/transform.js
@@ -74,8 +74,6 @@ export let fetchTE = (input, init) => () => fetch(input, init)
  * We either stream chat response as string + return it as E.right in the end, or return the error
  * as E.left
  *
- * 1
- *
  * @param stream - here we stream the E.right content as string before returning the E.right in the
  * end as well.
  * @param options.removeCodeBlock - if true, we trim the stream content between the first ```
